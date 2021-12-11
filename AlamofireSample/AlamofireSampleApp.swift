21925f98d0e852360cb6e83c4eb6861d84355537
yakintech <cagatayyildiz87@gmail.com>
2021-12-11 10:35:25 +0300

    Initial Commit

---
+++ AlamofireSample/AlamofireSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireSampleApp.swift
+//  AlamofireSample
+//
+//  Created by Çağatay Yıldız on 11.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlamofireSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlamofireSample
+//
+//  Created by Çağatay Yıldız on 11.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
